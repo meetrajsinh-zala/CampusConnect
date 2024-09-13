ea87878a9017202a2323c26e6125d7febbcbef65
Meetrajsinh Zala <zalameetraj07@gmail.com>
2024-09-13 20:25:28 +0530

    Created Login and Signup page

--- CampusConnect_FrontEnd/src/App.jsx
+++ CampusConnect_FrontEnd/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Outlet} from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="w-full h-full"><Outlet /></div>;
 };
 
 export default App;
---
+++ CampusConnect_FrontEnd/src/components/SignUp.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import {Label} from './ui/label';
+import {Input} from './ui/input';
+import {Button} from './ui/button';
+import {Link} from 'react-router-dom';
+
+const SignUp = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>Welcome !</CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
+            <Label>First Name</Label>
+            <Input type="text" placeholder="EX : John" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Last Name</Label>
+            <Input type="text" placeholder="EX : Doe" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Username</Label>
+            <Input type="text" placeholder="EX : johndeo12" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Email</Label>
+            <Input type="email" placeholder="test@example.com" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Password</Label>
+            <Input type="password" placeholder="*******" />
+          </div>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-1">
+          <Button className="w-full">Sign up</Button>
+          <p className="text-center">
+            Don't have an account?&nbsp;
+            <Link to={'/Login'} className="text-[#1c284f] font-bold">
+              Login
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
